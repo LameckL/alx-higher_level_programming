732e3277a040786cb735da3a40539e3b80cf0b83
Lameck Kipruto <lameckkipruto1@.com>
2023-09-05 08:12:17 -0700

    fixing alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for alph in range(97, 123):
-    if chr(alph) is not 'q' and chr(alph) is not 'e':
+    if chr(alph) != 'e' and chr(alph) != 'q':
         print("{}".format(chr(alph)), end="")
