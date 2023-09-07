17eb36d4653e181d6123228f34925ebcd0906935
Lameck Kipruto <lameckkipruto1@.com>
2023-09-07 09:51:46 -0700

    fixing magic calculation

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,10 +3,10 @@ def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
 
     if a < b:
-        su = add(a, b)
+        c = add(a, b)
         for i in range(4, 6):
-            su = add(su, i)
-        return (su)
+            c = add(c, i)
+        return (c)
 
     else:
         return(sub(a, b))
