ece5759eef11e612eeae5ed4413816ba68e74c6e
LameckL <88710250+LameckL@users.noreply.github.com>
2023-09-09 21:42:30 +0300

    space fix 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,5 +2,7 @@
 def multiple_returns(sentence):
     """func returning str length and its first char"""
     if sentence == "":
+        
         return (0, None)
+        
     return (len(sentence), sentence [0])
