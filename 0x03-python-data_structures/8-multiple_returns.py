831b41380e6ca00559670ba198e01259e7ed5fa5
LameckL <88710250+LameckL@users.noreply.github.com>
2023-09-09 20:29:31 +0300

    fixing 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     """func returning str length and its first char"""
-    if line == "":
+    if sentence == "":
 
-        return (0, None)
-
-    return (len(line), line[0])
+    return (len(sentence), sentence [0])
