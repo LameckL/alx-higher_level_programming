9388de82cf99b47ed7a096bf1a891075d34fe197
Lameck Kipruto <lameckkipruto1@.com>
2023-09-13 11:53:20 -0700

    update dictinary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+
+    """this func add/replace key/value in a dictionary"""
+
+    a_dictionary[key] = value
+
+    return (a_dictionary)
