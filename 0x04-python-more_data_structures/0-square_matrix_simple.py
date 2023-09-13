eac450473096b8d67209fc0751a7237c015e1cc9
Lameck Kipruto <lameckkipruto1@.com>
2023-09-13 01:49:50 -0700

    square matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+
+    mtrx = matrix.copy()
+
+    for i in range(len(matrix)):
+
+        mtrx[i] = list(map(lambda x: x**2, matrix[i]))
+
+        return (mtrx)
