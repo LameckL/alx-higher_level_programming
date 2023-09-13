d0de542fdb739f6b720b6235b5b2af38efbabcb2
Lameck Kipruto <lameckkipruto1@.com>
2023-09-13 12:28:10 -0700

    fixing multiply by2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-	"""func returning new dictionary with all values multiplied by 2"""
-    new_dict = a_dictionary.copy()
 
-    list_keys = list(new_dict.keys())
+    newd = a_dictionary.copy()
+
+    list_keys = list(newd.keys())
 
     for i in list_keys:
 
-        new_dict[i] *= 2
+        newd[i] *= 2
 
-    return (new_dict)
+    return (newd)
