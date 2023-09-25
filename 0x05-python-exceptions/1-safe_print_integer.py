f044f4b0db055dcdecb2d2a504ee34af3357f1c4
Lameck Kipruto <lameckkipruto1@.com>
2023-09-25 11:28:54 -0700

    1 safe print

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-"""func printing an integer with "{:d}".format()
-Args:
-integer value to print.
-Returns:
-if value has been correctly printed - True
-Otherwise - False
-"""
+    """func printing an integer with "{:d}".format()
+    Args:
+    integer value to print.
+    Returns:
+    if value has been correctly printed - True
+    Otherwise - False
+    """
 try:
-print("{:d}".format(value))
-return (True)
+    print("{:d}".format(value))
+    return (True)
 except (TypeError, ValueError):
-return (False)
+    return (False)
