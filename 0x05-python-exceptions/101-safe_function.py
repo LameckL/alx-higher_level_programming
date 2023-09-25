239a042a8580ff4f0445728a4d623eecf43a182a
LameckL <88710250+LameckL@users.noreply.github.com>
2023-09-25 20:17:50 +0100

    updating 101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -11,7 +11,7 @@ def safe_function(fct, *args):
   """
 try:
   result = fct(*args)
-return (result)
+  return (result)
 except:
   print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-return (None)
+  return (None)
