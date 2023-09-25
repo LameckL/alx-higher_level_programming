98b258691bfe88302d4a50d2e6037b2c48bd7363
LameckL <88710250+LameckL@users.noreply.github.com>
2023-09-25 20:14:11 +0100

    fix task 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 import sys
 def safe_print_integer_err(value):
-"""func that prints an integer
-Args:
-value (int): integer to print
-Returns:
-If value correctly printed - True
-Otherwise - False
+  """func that prints an integer
+  Args:
+    value (int): integer to print
+  Returns:
+    If value correctly printed - True
+    Otherwise - False
 """
 try:
-print("{:d}".format(value))
+  print("{:d}".format(value))
 return (True)
 except (TypeError, ValueError):
-print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+  print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
 return (False)
