a650071ba06bd030080db5d60fe078f73f7cd4cb
Lameck Kipruto <lameckkipruto1@.com>
2023-09-25 11:19:02 -0700

    fafe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-"""this func prints elememts of a list
-Args:
-my_list (list): list from which the elements are being print from
-x (int): no. of elements of my_list to print
-Returns:
-no. of elements being printed
-"""
+    """this func prints elememts of a list
+    Args:
+    my_list (list): list from which the elements are being print from
+    x (int): no. of elements of my_list to print
+    Returns:
+    no. of elements being printed
+    """
 el = 0
 for i in range(x):
-try:
-print("{}".format(my_list[i]), end="")
-el += 1
-except IndexError:
-break
+    try:
+        print("{}".format(my_list[i]), end="")
+        el += 1
+    except IndexError:
+        break
 print("")
 return (el)
