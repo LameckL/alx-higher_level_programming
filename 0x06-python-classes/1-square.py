18b5fbde1f0e46b95321c223524ca67f99e12993
Lameck Kipruto <lameckkipruto1@.com>
2023-09-26 05:32:23 -0700

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class Square definition"""
+
+
+class Square:
+    """Square represantation"""
+
+    def __init__(self, size):
+        """initialization of a new Square
+
+        Args:
+            size (int): new square size
+        """
+        self.__size = size
