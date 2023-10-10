f82853e62e587a532882d8713832140032b271e0
LameckL <88710250+LameckL@users.noreply.github.com>
2023-10-10 15:11:03 +0100

    json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function definition"""
+import json
+
+
+def to_json_string(my_obj):
+    """func returning JSON representation of a string object"""
+    return json.dumps(my_obj)
