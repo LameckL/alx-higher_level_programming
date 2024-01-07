e1b76d42ce9f1f8d32e25cd02d9ec5ccb185ecfd
LameckL <88710250+LameckL@users.noreply.github.com>
2024-01-07 15:23:49 +0000

    git commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""lists 10 recent commits on a GitHub repo
+Usage: ./100-github_commits.py <repository name> <repository owner>
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    re = requests.get(url)
+    commits = re.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
