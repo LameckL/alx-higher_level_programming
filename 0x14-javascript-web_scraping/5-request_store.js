2c557197ac2cf6d23ba52b12fc6ad2dcc208b0a0
Lameck Kipruto <88710250+LameckL@users.noreply.github.com>
2024-01-30 16:40:30 +0000

    fix 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const fle = require('flwe');
+const fle = require('fle');
 const request = require('request');
 request(process.argv[2]).pipe(fle.createWriteStream(process.argv[3]));
