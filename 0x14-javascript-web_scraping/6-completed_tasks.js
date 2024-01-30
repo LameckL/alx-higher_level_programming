14d327301167f321fd7ba785bdbd7f52a7ab4134
Lameck Kipruto <lameckkipruto1.com>
2024-01-30 08:32:18 -0800

    3 to 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+
+request(process.argv[2], function (err, response, body) {
+  if (err == null) {
+    const respo = {};
+    const json = JSON.parse(body);
+    for (let i = 0; i < json.length; i++) {
+      if (json[i].completed === true) {
+        if (respo[json[i].userId] === undefined) {
+          resp[json[i].userId] = 0;
+        }
+        respo[json[i].userId]++;
+      }
+    }
+    console.log(respo);
+  }
+});
